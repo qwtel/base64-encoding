e1aa9d50e853f79b2c0d180e33f45742b2f87351
Florian Klampfer <mail@qwtel.com>
2020-08-15 19:26:34 +0700

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import { uglify } from "rollup-plugin-uglify";
+// import { uglify } from "rollup-plugin-uglify";
 
 export default [
+  // {
+  //   input: 'cjs/index.cjs',
+  //   output: {
+  //     file: `dist/index.js`,
+  //     format: 'umd',
+  //     name: 'base64Encoding',
+  //     sourcemap: true
+  //   },
+  //   plugins: [
+  //     resolve(),
+  //     commonjs(),
+  //     uglify(),
+  //   ],
+  // }, 
   {
-    input: 'cjs/index.cjs',
+    input: 'index.js',
     output: {
-      file: `dist/index.js`,
-      format: 'umd',
-      name: 'base64Encoding',
-      sourcemap: true
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      uglify(),
-    ],
-  }, 
-  {
-    input: 'mjs/index.js',
-    output: {
-      file: `dist/module.js`,
+      file: `module/index.js`,
       format: 'es',
       sourcemap: true
     },
